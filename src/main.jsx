780526158debcaba6beb5ac8f6a32cff1fe7cb41
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-12-12 13:58:48 +0600

    email functionality done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import MainLayout from './layout/MainLayout'
 import 'react-circular-progressbar/dist/styles.css';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainLayout/>
+    <div>
+      <MainLayout />
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+      />
+    </div>
   </React.StrictMode>,
 )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,23 @@
+import emailjs from '@emailjs/browser';
 import Title from "../components/Header/Title";
 import { FaTelegramPlane } from "react-icons/fa";
+import toast from 'react-hot-toast';
 
 
 const Contact = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const messagebody ={
+            from_name: e.target?.name.value,
+            email_id: e.target?.email.value,
+            message: e.target?.message.value
+        }
+        console.log(messagebody)
+        emailjs.send('service_50o4blc', 'template_0qmjs9s', messagebody,'lzY0dF7nJF3WgWDUi')
+        .then((result) => {
+            toast.success('Message Send Successfully!! Wait for reply')
+        })
+    }
     return (
         <div>
             <div className="px-7 py-5">
@@ -35,21 +50,21 @@ const Contact = () => {
                     <Title title='Get in Touch' />
                 </div>
                 <div className="border-[0.5px] border-[#fff]/10"></div>
-                <form className="py-4 px-7">
+                <form onSubmit={handleSubmit} className="py-4 px-7">
                     <div className="grid grid-cols-2 gap-2">
                         <div>
-                            <input className="w-full  px-5 py-2 rounded-md border-[#949B9B]/50 focus:outline-[#F6B846] focus:outline-1" type="text" placeholder="Your Name" />
+                            <input name="name" className="w-full  px-5 py-2 rounded-md border-[#949B9B]/50 focus:outline-[#F6B846] focus:outline-1" type="text" placeholder="Your Name" />
                         </div>
                         <div>
-                            <input className="w-full focus:outline-[#F6B846]  px-5  py-2 rounded-md border-[#949B9B]/50 focus:outline-1" type="text" placeholder="Your Email" />
+                            <input name="email" className="w-full focus:outline-[#F6B846]  px-5  py-2 rounded-md border-[#949B9B]/50 focus:outline-1" type="text" placeholder="Your Email" />
                         </div>
                     </div>
                 
                     <div className="pt-5">
-                        <textarea className="w-full h-[80px] px-5 py-2 rounded-md border-[#949B9B]/50 focus:outline-[#F6B846] focus:outline-1" type="text" placeholder="Message"></textarea>
+                        <textarea name="message" className="w-full h-[80px] px-5 py-2 rounded-md border-[#949B9B]/50 focus:outline-[#F6B846] focus:outline-1" type="text" placeholder="Message"></textarea>
                     </div>
                     <div className="pt-5">
-                        <button className="w-full py-[2px] h-[30px] rounded-md border-[#949B9B]/50 border-2 border-[#F6B846] hover:bg-[#F6B846] text-[#F6B846] duration-200 hover:text-[#222222] font-bold flex justify-center items-center gap-3">Send Message <FaTelegramPlane color="#fff"/></button>
+                        <button type="submit" className="w-full py-[2px] h-[30px] rounded-md border-[#949B9B]/50 border-2 border-[#F6B846] hover:bg-[#F6B846] text-[#F6B846] duration-200 hover:text-[#222222] font-bold flex justify-center items-center gap-3">Send Message <FaTelegramPlane color="#fff"/></button>
                     </div>
                 </form>
             </div>
